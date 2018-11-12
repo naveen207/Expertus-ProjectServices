b24f0723e4d96a1b2467bd0c6098c57b850b5457
naveen singh <naveensingh881@gmail.com>
2018-11-12 13:01:12 -0500

    First commit

---
+++ Expertus-ProjectServices/src/main/java/com/expertus/web/ExpertusProjectServicesApplication.java
@@ -0,0 +1,12 @@
+package com.expertus.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertusProjectServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpertusProjectServicesApplication.class, args);
+	}
+}
